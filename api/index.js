18652bac1b5df4e3bcf8860bef307678e9b3ec2d
cddmaceio <roniercassio12@gmail.com>
2026-04-21 17:59:19 -0300

    feat(api): refactor app loading mechanism to use dynamic import and promise handling

--- api/index.js
+++ api/index.js
@@ -1,11 +1,22 @@
-import app from "../apps/api/dist/app.mjs";
+let appPromise;
 
-export const config = {
+function loadApp() {
+  if (!appPromise) {
+    appPromise = import("../apps/api/dist/app.mjs").then((mod) => mod.default);
+  }
+
+  return appPromise;
+}
+
+module.exports = async function handler(req, res) {
+  const app = await loadApp();
+  return app(req, res);
+};
+
+module.exports.config = {
   api: {
     bodyParser: false,
     externalResolver: true,
     responseLimit: false,
   },
 };
-
-export default app;
